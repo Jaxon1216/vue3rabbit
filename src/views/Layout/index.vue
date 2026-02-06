8771dec46a201029c12cb0e57f608cb3e2cf883f
Jaxon <jiangxu05@outlook.com>
2026-02-06 21:38:35 +0800

    docs: 字体图标引入

--- src/views/Layout/index.vue
+++ src/views/Layout/index.vue
@@ -5,6 +5,8 @@ import LayoutFooter from './components/LayoutFooter.vue'
 </script>
 
 <template>
+<!-- <i class="iconfont icon-xinlangweibo"></i> -->
+<!-- <link rel="stylesheet" href="https://at.alicdn.com/t/c/font_5124612_0cbvjnqbss6.css"> -->
   <LayoutNav />
   <LayoutHeader />
   <RouterView />
