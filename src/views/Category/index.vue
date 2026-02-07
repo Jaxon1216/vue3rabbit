6d374138089d9f040c2797441d7ffa142505607e
Jaxon <jiangxu05@outlook.com>
2026-02-07 22:41:05 +0800

    docs: 激活状态控制和分类列表

--- src/views/Category/index.vue
+++ src/views/Category/index.vue
@@ -4,7 +4,7 @@ import { useRoute } from 'vue-router'
 import { onMounted, ref } from 'vue'
 
 import { getBannerAPI } from '@/apis/home'
-
+import GoodsItem from '../Home/components/GoodsItem.vue'
 //获取数据
 const categoryData = ref({})
 const route = useRoute()
@@ -37,12 +37,34 @@ onMounted(() => getBanner())
           <el-breadcrumb-item>{{ categoryData.name }}</el-breadcrumb-item>
         </el-breadcrumb>
       </div>
+      <!-- 轮播图 -->
       <div class="home-banner">
         <el-carousel height="500px">
           <el-carousel-item v-for="item in bannerList" :key="item.id">
             <img :src="item.imgUrl" alt="">
           </el-carousel-item>
         </el-carousel>
+      </div>
+      <!-- 全部分类 -->
+      <div class="sub-list">
+        <h3>全部分类</h3>
+        <ul>
+          <li v-for="i in categoryData.children" :key="i.id">
+            <RouterLink to="/">
+              <img :src="i.picture" />
+              <p>{{ i.name }}</p>
+            </RouterLink>
+          </li>
+        </ul>
+      </div>
+      <!-- 分类关联商品 -->
+      <div class="ref-goods" v-for="item in categoryData.children" :key="item.id">
+        <div class="head">
+          <h3>- {{ item.name }}-</h3>
+        </div>
+        <div class="body">
+          <GoodsItem v-for="good in item.goods" :goods="good" :key="good.id" />
+        </div>
       </div>
     </div>
   </div>
