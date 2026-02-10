6db8b61923643c80bdcdd58d17fdd123a8e1de52
Jaxon <jiangxu05@outlook.com>
2026-02-10 16:21:46 +0800

    docs: 简单表单校验

--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -1,5 +1,26 @@
 <script setup>
 
+//表单校验（账号名+密码）
+import { ref } from 'vue'
+
+//1. 准备表单对象
+const form = ref({
+  account: '',
+  password: '',
+  agree: true
+})
+
+//2. 准备规则对象
+const rules = {
+  account: [{ required: true, message: '用户名不能为空', trigger: 'blur' }],
+  //require表示不能为空
+  password: [
+    { required: true, message: '密码不能为空', trigger: 'blur' },
+    { min: 6, max: 14, message: '密码长度为6-14个字符', trigger: 'blur' },
+  ],
+}
+
+
 </script>
 
 
@@ -24,13 +45,14 @@
         </nav>
         <div class="account-box">
           <div class="form">
-            <el-form label-position="right" label-width="60px"
+            <el-form ref="formRef" :model="form" :rules="rules" label-position="right" label-width="60px"
               status-icon>
-              <el-form-item  label="账户">
-                <el-input/>
+              <el-form-item  prop="account" label="账户">
+                <!-- 第三步：制定表单的校验字段名 prop -->
+                <el-input v-model="form.account"/>
               </el-form-item>
-              <el-form-item label="密码">
-                <el-input/>
+              <el-form-item prop="password" label="密码">
+                <el-input v-model="form.password" />
               </el-form-item>
               <el-form-item label-width="22px">
                 <el-checkbox  size="large">
