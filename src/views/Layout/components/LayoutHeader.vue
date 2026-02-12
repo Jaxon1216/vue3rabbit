5af639438d2e97e789dd4cceb7eacaa39fd20998
Jaxon <jiangxu05@outlook.com>
2026-02-12 21:17:48 +0800

    docs: 头部购物车渲染，列表

--- src/views/Layout/components/LayoutHeader.vue
+++ src/views/Layout/components/LayoutHeader.vue
@@ -1,5 +1,6 @@
 <script setup>
-
+import LayoutHeaderUI from './LayoutHeaderUI.vue';
+import HeaderCart from './HeaderCart.vue';
 
 //使用pinia中的数据
 import { useCategoryStore } from '@/stores/category'
@@ -13,20 +14,13 @@ const categoryStore = useCategoryStore()
       <h1 class="logo">
         <RouterLink to="/">小兔鲜</RouterLink>
       </h1>
-      <ul class="app-header-nav">
-        <li class="home">
-          <RouterLink to="/">首页</RouterLink>
-        </li>
-        <li class="home" v-for="item in categoryStore.categoryList" :key="item.id">
-          <RouterLink active-class="active" :to="`/category/${item.id}`">{{ item.name }}</RouterLink>
-        </li>
-      </ul>
+      <LayoutHeaderUI />
       <div class="search">
         <i class="iconfont icon-search"></i>
         <input type="text" placeholder="搜一搜">
       </div>
       <!-- 头部购物车 -->
-      
+      <HeaderCart />
     </div>
   </header>
 </template>
